74c0cd07f7d1c27c9b0c564ebcc0537ee764f2d2
zengjiawan <986738389@qq.com>
2020-01-03 20:19:05 +0800

    首页轮播图实现

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,49 @@
-import React ,{Fragment}from 'react'
+import React, { Component } from 'react'
+import axios from "axios"
+ import { Carousel, WingBlank } from 'antd-mobile';
+export default class Index extends Component {
+    state={
+        banner:[],
+        imgHeight: 176
 
-export default function Home() {
-    return (
-        <div>
-             首页
-        </div>
-    )
+    }
+    componentDidMount(){
+      axios.get("http://hkzf.zbztb.cn/home/swiper").then((res)=>{
+          console.log(res);
+          this.setState({
+            banner:res.data.body
+          })
+    })
+    }
+    render() {
+        return (
+            <WingBlank>
+            <Carousel
+              autoplay
+              infinite
+            >
+              {this.state.banner.map(val => (
+                  val.imgSrc="http://hkzf.zbztb.cn"+val.imgSrc,
+                <a
+                  key={val}
+                  href=""
+                  style={{ display: 'inline-block', width: '100%', height: this.state.imgHeight }}
+                >
+                  <img
+                    src={val.imgSrc}
+                    alt=""
+                    style={{ width: '100%', verticalAlign: 'top' }}
+                    onLoad={() => {
+                      // fire window resize event to change height
+                    //   window.dispatchEvent(new Event('resize'));
+                    //   this.setState({ imgHeight: 'auto' });
+                    }}
+                  />
+                </a>
+              ))}
+            </Carousel>
+          </WingBlank>
+        )
+    }
 }
+
